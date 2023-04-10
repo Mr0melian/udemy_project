ed094c06e85756f964e92b26769802bc9dab675e
Ivan <serhieievivan6@gmail.com>
2023-04-10 16:02:12 +0300

    added TCP

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,7 @@
+use std::net::TcpListener;
+use std::convert::TryFrom;
+use super::http::Request;
+use std::io::Read;
 pub struct Server{
     addr: String,
 }
@@ -10,5 +14,31 @@ impl Server {
 
     pub fn run(self){
         println!("listing on {}", self.addr);
+        let listener = match TcpListener::bind(&self.addr){
+            Ok(listener) => listener,
+            Err(err) => panic!("can`t be listened by this addr")
+        };
+
+
+        loop {
+            match listener.accept() {
+                Ok((mut stream, _)) => {
+                    let mut bufferr = [0; 1024];
+                    match stream.read(&mut bufferr){
+                        Ok(_) => {
+                            println!("Received a request: {}", String::from_utf8_lossy(&bufferr));
+
+                           
+                            let res = match Request::try_from(&bufferr as &[u8]){
+                                Ok(res) =>{}
+                                Err(e)=> println!("Feild to parse a request: {}", e),
+                            };
+                        }
+                        Err(e) => println!("Feiled to read from connection:{}", e),
+                    }
+                }
+                Err(e)=> println!("error: {}", e),
+            }
+        }
     }
 }
